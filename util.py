2f81ec721c181b04c076b41d83aa921778a63d2c
ssaemo <jeontha@gmail.com>
2017-12-10 00:03:04 +0900

    first commit

---
+++ util.py
@@ -0,0 +1,23 @@
+from random import random
+
+def parse_url(SERVER):
+    temp = SERVER.split(':')
+    if len(temp) != 1:
+        port = int(temp[1])
+    else:
+        port = 6667
+    return temp[0], port
+
+def random_list(n=10):
+    temp = list(range(n))
+    result = []
+    while len(temp) > 0:
+        idx = int(random() * len(temp))
+        result.append(temp[idx])
+        del temp[idx]
+    return result
+
+def swap(arr, a, b):
+    temp = arr[a]
+    arr[a] = arr[b]
+    arr[b] = temp
